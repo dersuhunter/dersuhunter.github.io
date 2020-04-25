3d6c07a364b2e46877d1701e22fa9b27afe6b24e
dersuhunter <64291411+dersuhunter@users.noreply.github.com>
2020-04-25 09:27:48 -0500

    Update maze.js

--- js/maze.js
+++ js/maze.js
@@ -1,4 +1,14 @@
 "use strict";
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+};
+
+
 const uuidv4 = ()=>"yxx-xxx-xxxxx-xxxxxxx-xxxxxxxxxxx".replace(/[xy]/g, 
 function(r) {
         var t = 16 * Math.random()|0;
@@ -10,12 +20,12 @@ function(r) {
         t.searchParams.set("id", r), Math.seedrandom(r), window.history.replaceState(null, "Maze", t.href)
     },
 
-    LEVELS={
-        basico:{W:15,H:25},
-        intermedio:{W:20,H:25},
-        avanzado:{W:30,H:35},
-        locura:{W:50,H:},
-	mitico:{W:80,H:70},
+   LEVELS={
+	basico:{W:15,H:15},
+        intermedio:{W:20,H:20},
+        avanzado:{W:30,H:30},
+        locura:{W:50,H:50},
+	mitico:{W:80,H:80},
 	god:{W:100,H:100}
     },
     draw_maze=r=>{
@@ -86,7 +96,7 @@ function(r) {
 		v(r,t,e) {this.ml(r+e,t,r+e,t+s)}
         	}
 
-        let R=new z("#000000",1.5,"1.0"), U=new z(randomcolor,8,"1.0"), k=new z(randomcolor,5,"0.1");
+        let R=new z("#000000",1.5,"1.0"), U=new z(randomcolor,3,"1.0"), k=new z(randomcolor,3,"0.1");
 
         const 
 	    P=(r,t,e=p)=>{R.m(r+p,t),R.a(r,t,e,0,o)},
@@ -94,87 +104,121 @@ function(r) {
             D=(r,t,e=p)=>{R.m(r+s,t+p),R.a(r+s,t,e,o,l)},
             H=(r,t,e=p)=>{R.m(r+x,t+s),R.a(r+s,t+s,e,l,n)},
 
+    // │ │
+    // │ │
             Y=(r,t)=>{
 		R.v(r,t,p)
 		R.v(r,t,x)
 		},
+    // ───
+    // ───
             $=(r,t)=>{
 		R.h(r,t,p),
 		R.h(r,t,x)
 		},
+    // │ │
+    // └─┘
             W=(r,t)=>{
 		R.m(r+x,t,m),
 		R.l(r+x, t+x),
 		R.l(r+p, t+x),
 		R.l(r+d,t)
 		},
+    // ┌─┐
+    // │ │
             V=(r,t)=>{
 		R.m(r+x,t+s,m)
 		R.l(r+x, t+p),
 		R.l(r+p, t+p),
 		R.l(r+p,t+s)
 		},
+    // ┌──
+    // └──
+
             j=(r,t)=>{
 		R.m(r+s,t+x),
 		R.l(r+d,t+x);
 		R.l(r+p,t+p),
 		R.l(r+s,t+p)
 		},
+    // ──┐
+    // ──┘
             T=(r,t)=>{
 		R.m(r,t+x),
 		R.l(r+x,t+x),
 		R.l(r+x,t+p),
 		R.l(r,t+p)
 		},
+    //  │ └
+    //  └──
             q=(r,t)=>{
 		D(r,t),
 		R.m(r+s,t+x),
 		R.a(r+x,t+p,y,o,l),
 		R.l(r+p,t)
 		},
+    // ┌──
+    // │ ┌
             F=(r,t)=>{
 		H(r,t),
 		R.m(r+s,t+p),
 		R.a(r+x,t+x,y,n,l,0),
 		R.l(r+p,t+s)
 		},
+    //  ┘ │
+    //  ──┘
             J=(r,t)=>{
 		P(r,t),
 		R.m(r+x,t),
 		R.a(r+p,t+p,y,0,o),
 		R.l(r,t+x)
 		},
+    // ──┐
+    // ┐ │
             N=(r,t)=>{
 		_(r,t),
 		R.m(r,t+p),
 		R.a(r+p,t+x,y,n,a),
 		R.l(r+x,t+s)
 		},
+    // │ └
+    // │ ┌
             Q=(r,t)=>{
 		R.v(r,t,p),
 		D(r,t),
 		H(r,t)
 		},
+     // ┘ │
+     // ┐ │
             X=(r,t)=>{
 		R.v(r,t,x),
 		P(r,t),
 		_(r,t)
 		},
+
+    // ┘ └
+    // ───
             tr=(r,t)=>{
 		R.h(r,t,x),
 		P(r,t),
 		D(r,t)
 		},
+    // ───
+    // ┐ ┌
             er=(r,t)=>{
 		R.h(r,t,p),
 		_(r,t),H(r,t)
 		},
+    // ┘ └
+    // ┐ ┌
             lr=(r,t)=>{
 		P(r,t),
 		_(r,t),
 		D(r,t),
 		H(r,t)
 		},
+    // ┤ ├
+    // ┤ ├
             or=(r,t)=>{
 		R.v(r,t,p),
 		R.v(r,t,x),
@@ -183,6 +227,8 @@ function(r) {
 		R.mh(r+s,t+p,x-s),
 		R.mh(r+s,t+x,x-s)
 		},
+    // ┴─┴
+    // ┬─┬
             ar=(r,t)=>{
 		R.h(r,t,p),
 		R.h(r,t,x),
@@ -301,6 +347,7 @@ function(r) {
             change_id();
             let t=new URL(window.location);
             t.searchParams.set("nivel",r),t.searchParams.set("ruta",""),window.history.replaceState(null,"Maze",t.href);
+	    
             const e=create_maze(LEVELS[r].W,LEVELS[r].H);
             draw_maze(e)
         };
@@ -385,16 +432,12 @@ function(r) {
         })(r,t,.5)
     },
 
-
-    init = ()=>{
+    init=()=>{
         const r = new URL(window.location);
-
         let t = r.searchParams.get("nivel");
-        null===t && (t = "intermedio"), r.searchParams.set("nivel", t), window.history.replaceState(null, "Maze", r.href);
-
+        null===t && (t = 'basico'),r.searchParams.set("nivel", t), window.history.replaceState(null, "Maze", r.href);
         const e = r.searchParams.get("id");
         null===e ? change_id() :Math.seedrandom(e);
-
-        const l = create_maze(LEVELS[t].W, LEVELS[t].H);
-        draw_maze(l)
+	const l = create_maze(LEVELS[t].W, LEVELS[t].H);
+	draw_maze(l)
     };
