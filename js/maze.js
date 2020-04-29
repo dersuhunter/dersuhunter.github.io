5b2ba0403359ebefc2aa467344275a03d1c2ee79
dersuhunter <64291411+dersuhunter@users.noreply.github.com>
2020-04-28 22:02:17 -0500

    Update maze.js

--- js/maze.js
+++ js/maze.js
@@ -435,7 +435,7 @@ function(r) {
         let w=new Set, f=new Set;
         const g=(r,t)=>f[[r.x,r.y]]===f[[t.x,t.y]], p=(r,t)=>{const e=f[[r.x,r.y]],l=f[[t.x,t.y]];w[e].add(l); const o=w[l];w[e]=new Set([...w[e],...o]),w[l]=new Set,w[e].forEach(r=>{f[r]=e})};
 	
-	var complex= [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,0.09,0.08,0.07,0.06,0.05,0.04,0.03,0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0]; 
+	var complex= [0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.09,0.08,0.07,0.06,0.05,0.04,0.03,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]; 
 	const randomcomplex = complex[Math.floor(Math.random() * complex.length)];
 	    
 
